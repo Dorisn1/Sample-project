73ee4c7e1e8517cc831b30dac56b50e7ec085123
Dorota <dorota.zebrowska_n1@wp.pl>
2023-01-09 12:31:49 +0100

    zadania od 05-06

---
+++ src/main/java/pl/coderslab/apierwszyprogram/Main06.java
@@ -0,0 +1,9 @@
+package pl.coderslab.apierwszyprogram;
+
+public class Main06 {
+
+    public static void main(String[] args) {
+//        System.out.println("Hello world!")
+    }
+}
+/** było brak średnika (;) za "Hello world" */
